d81d2bff05d79afcf04fc5adac8bcdd84b2914e2
Olha Prokhorova <prokhorova.olha@gmail.com>
2023-09-02 00:16:54 +0300

    added lambdas

---
+++ lambda/hello.py
@@ -0,0 +1,11 @@
+import json
+
+def handler(event, context):
+    print('request: {}'.format(json.dumps(event)))
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/plain'
+        },
+        'body': 'Good morning man! You have hit a  {}\n'.format(event['path'])
+    }
